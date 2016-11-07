2026caf20f52335992f8f9eb417d7a0a9fbfee2c
Fabien Steines <fabien.steines@gmail.com>
2016-11-07 20:22:08 +0100

    close on esc

--- src/main/java/lu/innocence/ignis/view/CreateProjectDialog.java
+++ src/main/java/lu/innocence/ignis/view/CreateProjectDialog.java
@@ -1,10 +1,13 @@
 package lu.innocence.ignis.view;
 
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -32,7 +35,15 @@ public class CreateProjectDialog extends Stage{
 
     private void buildGUI() {
         BorderPane root = new BorderPane();
-        this.setScene(new Scene(root, 340, 270));
+        Scene scene = new Scene(root,340, 270);
+        this.setScene(scene);
+
+
+        scene.addEventHandler(KeyEvent.KEY_PRESSED, t -> {
+            if(t.getCode()== KeyCode.ESCAPE) {
+                this.close();
+            }
+        });
 
         GridPane grid = new GridPane();
         grid.setHgap(10);
@@ -82,7 +93,6 @@ public class CreateProjectDialog extends Stage{
         GridPane.setColumnSpan(companyTextField, 2);
 
         root.setCenter(grid);
-
 
         // Box on the Bottom
         HBox bottomBar = new HBox();
