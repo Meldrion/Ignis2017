9af2d5cc6a2bd018f67b10a045ba0add408a49ef
Fabien Steines <fabien.steines@gmail.com>
2016-11-22 17:01:00 +0100

    update

--- src/main/java/lu/innocence/ignis/view/gamedb/TilesetTab.java
+++ src/main/java/lu/innocence/ignis/view/gamedb/TilesetTab.java
@@ -1,12 +1,14 @@
 package lu.innocence.ignis.view.gamedb;
 
 import javafx.geometry.Insets;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
-import javafx.scene.layout.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import lu.innocence.ignis.component.TilesetManagerCanvas;
 import lu.innocence.ignis.engine.Tileset;
@@ -30,19 +32,19 @@ public class TilesetTab extends GameDBTab {
         GridPane topGrid = new GridPane();
         topGrid.setHgap(10);
         topGrid.setVgap(10);
-        topGrid.setPadding(new Insets(10,0,10,0));
+        topGrid.setPadding(new Insets(10, 0, 10, 0));
         topGrid.setMaxWidth(Integer.MAX_VALUE);
 
         Text tilesetName = new Text();
         tilesetName.setText("Tileset Name:");
-        topGrid.add(tilesetName,0,0);
+        topGrid.add(tilesetName, 0, 0);
         TextField edtTilesetName = new TextField();
         edtTilesetName.setMaxWidth(Integer.MAX_VALUE);
-        topGrid.add(edtTilesetName,1,0);
+        topGrid.add(edtTilesetName, 1, 0);
 
         Text lblTilesetImage = new Text();
         lblTilesetImage.setText("Tileset Image:");
-        topGrid.add(lblTilesetImage,0,1);
+        topGrid.add(lblTilesetImage, 0, 1);
 
         HBox tilesetImageInputAndButtonLayout = new HBox();
         tilesetImageInputAndButtonLayout.setSpacing(10);
@@ -54,8 +56,8 @@ public class TilesetTab extends GameDBTab {
         Button btnLookForTilesetImage = new Button();
         btnLookForTilesetImage.setText("...");
 
-        tilesetImageInputAndButtonLayout.getChildren().addAll(edtTilesetImage,btnLookForTilesetImage);
-        topGrid.add(tilesetImageInputAndButtonLayout,1,1);
+        tilesetImageInputAndButtonLayout.getChildren().addAll(edtTilesetImage, btnLookForTilesetImage);
+        topGrid.add(tilesetImageInputAndButtonLayout, 1, 1);
         centerBox.getChildren().add(topGrid);
 
         HBox.setHgrow(edtTilesetImage, Priority.ALWAYS);
@@ -63,24 +65,34 @@ public class TilesetTab extends GameDBTab {
 
 
         HBox centerHPanel = new HBox();
-        centerBox.setPadding(new Insets(10,10,10,10));
+        centerBox.setPadding(new Insets(10, 10, 10, 10));
+        centerBox.setSpacing(10);
         centerHPanel.setSpacing(10);
 
         // Terrain
         GridPane innerLeftPanel = new GridPane();
         Text lblTerrain = new Text();
         lblTerrain.setText("Terrain");
-        innerLeftPanel.add(lblTerrain,0,0,2,1);
-        TextField edtTerrain = new TextField();
-        innerLeftPanel.add(edtTerrain,0,1);
-        Button btnTerrain = new Button();
-        btnTerrain.setText("...");
-        innerLeftPanel.add(btnTerrain,1,1);
+
+        innerLeftPanel.add(lblTerrain, 0, 0, 2, 1);
+        innerLeftPanel.setHgap(10);
+        innerLeftPanel.setVgap(10);
+
+        for (int i = 0; i < 8; i++) {
+            TextField edtTerrain = new TextField();
+            innerLeftPanel.add(edtTerrain, 0, 1 + i);
+
+            Button btnTerrain = new Button();
+            btnTerrain.setText("...");
+            innerLeftPanel.add(btnTerrain, 1, 1 + i);
+        }
+
         centerHPanel.getChildren().add(innerLeftPanel);
 
         // TilesetCanvas with Scrollbox
         ScrollPane tilesetScroller = new ScrollPane();
         tilesetScroller.setPrefWidth(280);
+        tilesetScroller.setMaxHeight(Integer.MAX_VALUE);
         TilesetManagerCanvas tsManagerCanvas = new TilesetManagerCanvas();
         tilesetScroller.setContent(tsManagerCanvas);
 
@@ -116,16 +128,19 @@ public class TilesetTab extends GameDBTab {
         centerHPanel.getChildren().add(rightBox);
         centerBox.getChildren().add(centerHPanel);
 
+        centerBox.setMaxHeight(Integer.MAX_VALUE);
+        VBox.setVgrow(centerHPanel,Priority.ALWAYS);
+
         this.setCenter(centerBox);
     }
 
     private void fromTilesetManager(TilesetManager tsManager) {
         this.contentList.getItems().clear();
         int max = tsManager.getTilesetList().size();
-        for (int i=0;i<max;i++) {
+        for (int i = 0; i < max; i++) {
             Tileset ts = this.tsManager.getTilesetAtIndex(i);
             if (ts != null) {
-                this.contentList.getItems().add(String.format("%s:%s",String.valueOf(i),ts.getName()));
+                this.contentList.getItems().add(String.format("%s:%s", String.valueOf(i), ts.getName()));
             }
         }
     }
