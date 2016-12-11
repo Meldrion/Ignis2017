0ffb0a56185c649838f11dd5cdb361a59e1f7bd9
Fabien Steines <fabien.steines@gmail.com>
2016-12-11 21:14:08 +0100

    update

--- src/main/java/lu/innocence/ignis/engine/Terrain.java
+++ src/main/java/lu/innocence/ignis/engine/Terrain.java
@@ -38,28 +38,33 @@ public class Terrain {
 
     public void draw(GraphicsContext g, int x, int y,Integer[][] sameMatrix) {
 
+        int top_left = sameMatrix[0][0];
+        int top = sameMatrix[1][0];
+        int top_right = sameMatrix[2][0];
+        int middle_left = sameMatrix[0][1];
+        int middle = IS_UNSET;
+        int middle_right = sameMatrix[2][1];
+        int bottom_left = sameMatrix[0][2];
+        int bottom = sameMatrix[1][2];
+        int bottom_right = sameMatrix[2][2];
 
-        if (sameMatrix[1][0] == IS_SAME && sameMatrix[0][1] == IS_SAME
-                && sameMatrix[2][1] == IS_SAME  && sameMatrix[1][2] == IS_SAME) {
+        if (top == IS_SAME && bottom == IS_SAME && middle_left == IS_SAME && middle_right == IS_SAME) {
             // Middle
             draw(g,x,y,1,2);
         } else {
-            if (sameMatrix[0][0] == IS_SAME && sameMatrix[0][1] == IS_SAME &&
-                    sameMatrix[1][2] == IS_SAME) {
+            if (top == IS_SAME && bottom == IS_SAME && middle_left == IS_SAME && middle_right == IS_DIFFERENT) {
                 // Middle Right
                 draw(g,x,y,2,2);
             } else {
-                if (sameMatrix[0][1] == IS_SAME && sameMatrix[1][2] == IS_SAME
-                        &&  sameMatrix[2][1] == IS_DIFFERENT) {
+                if (middle_left == IS_SAME && bottom == IS_SAME
+                        && top == IS_DIFFERENT && middle_right == IS_DIFFERENT) {
                     // Top right
                     draw(g,x,y,2,1);
                 } else {
                     // We are nothing
                     draw(g,x,y,0,0);
                 }
-
             }
-
         }
     }
 
