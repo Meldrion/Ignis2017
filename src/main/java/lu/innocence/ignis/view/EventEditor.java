81876988ea7ca97d22cf8c2bac2981fb8fd04a67
Fabien Steines <fabien.steines@gmail.com>
2017-01-27 17:40:34 +0100

    update

--- src/main/java/lu/innocence/ignis/view/EventEditor.java
+++ src/main/java/lu/innocence/ignis/view/EventEditor.java
@@ -1,9 +1,13 @@
 package lu.innocence.ignis.view;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -38,7 +42,27 @@ public class EventEditor extends Stage {
         Scene scene = new Scene(root);
         this.setScene(scene);
 
+        HBox topBox = new HBox();
+
+        VBox nameBox = new VBox();
+        Text nameLabel = new Text();
+        nameLabel.setText("Event Name");
+        TextField textFieldName = new TextField();
+        nameBox.getChildren().addAll(nameLabel,textFieldName);
+
+        VBox noteBox = new VBox();
+        Text noteLabel = new Text();
+        noteLabel.setText("Notes");
+        TextField textFieldNote = new TextField();
+        noteBox.getChildren().addAll(noteLabel,textFieldNote);
+
+        topBox.getChildren().addAll(nameBox,noteBox);
+        // Center Part
+        root.setTop(topBox);
+
+        // Button Box
         HBox hLayout = new HBox();
+        hLayout.setAlignment(Pos.BOTTOM_RIGHT);
 
         Button okButton = new Button();
         okButton.setText("Accept");
@@ -48,6 +72,8 @@ public class EventEditor extends Stage {
 
         hLayout.getChildren().addAll(okButton,cancelButton);
         root.setBottom(hLayout);
+
+
     }
 
 }
