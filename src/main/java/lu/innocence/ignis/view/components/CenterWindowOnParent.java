f6386d05beb264e3ceb5fe986e8efd5f7632e5d2
Fabien Steines <fabien.steines@gmail.com>
2017-02-15 17:52:00 +0100

    code improvements

--- src/main/java/lu/innocence/ignis/view/components/CenterWindowOnParent.java
+++ src/main/java/lu/innocence/ignis/view/components/CenterWindowOnParent.java
@@ -4,6 +4,9 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.stage.Stage;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @author Fabien Steines
  *         mailto:fabien.steines@post.lu
@@ -18,20 +21,49 @@ public class CenterWindowOnParent {
 
     public static void center(Stage parent,Stage toBeCentered) {
 
+        AtomicInteger xPos = new AtomicInteger();
+        AtomicInteger yPos = new AtomicInteger();
+        AtomicBoolean xDone = new AtomicBoolean(false);
+        AtomicBoolean yDone = new AtomicBoolean(false);
+
         ChangeListener<Number> listenerW = (observable, oldValue, newValue) -> {
             double newX = (parent.getWidth() - newValue.doubleValue()) / 2 + parent.getX();
-            toBeCentered.setX(newX);
+            xPos.set((int)newX);
+            xDone.set(true);
+            if (xDone.get() && yDone.get()) {
+                toBeCentered.setX(xPos.doubleValue());
+                toBeCentered.setY(yPos.doubleValue());
+                CenterWindowOnParent.setPosition(xDone,yDone,xPos,yPos,toBeCentered);
+            }
         };
         toBeCentered.widthProperty().addListener(listenerW);
 
         ChangeListener<Number> listenerH = (observable, oldValue, newValue) -> {
             double newY = (parent.getHeight() - newValue.doubleValue()) / 2 + parent.getY();
-            toBeCentered.setY(newY);
+            yPos.set((int)newY);
+            yDone.set(true);
+            CenterWindowOnParent.setPosition(xDone,yDone,xPos,yPos,toBeCentered);
         };
 
         toBeCentered.widthProperty().addListener(listenerW);
         toBeCentered.heightProperty().addListener(listenerH);
+    }
 
+    /**
+     *
+     * @param xDone
+     * @param yDone
+     * @param xPos
+     * @param yPos
+     * @param toBeCentered
+     */
+    private static void setPosition(AtomicBoolean xDone,AtomicBoolean yDone,
+                                    AtomicInteger xPos,AtomicInteger yPos,
+                                    Stage toBeCentered) {
+        if (xDone.get() && yDone.get()) {
+            toBeCentered.setX(xPos.doubleValue());
+            toBeCentered.setY(yPos.doubleValue());
+        }
     }
 
 }
