ac37af29dc28fbeee49f023dd6f922a0b5a80430
Fabien Steines <fabien.steines@gmail.com>
2017-02-16 15:42:20 +0100

    code improvements

---
+++ src/main/java/lu/innocence/ignis/engine/Event.java
@@ -0,0 +1,35 @@
+package lu.innocence.ignis.engine;
+
+import javafx.scene.canvas.GraphicsContext;
+
+/**
+ * @author Fabien Steines
+ *         mailto:fabien.steines@post.lu
+ *         Copyright by POST Technologies
+ *         <p>
+ *         <p>
+ *         Last revision - $(DATE) - Fabien Steines
+ */
+public class Event {
+
+    private int xPos;
+    private int yPos;
+    private String name;
+
+    public Event(int x,int y,String name) {
+        this.setPosition(x,y);
+    }
+
+    public void setPosition(int x,int y) {
+        this.xPos = x;
+        this.yPos = y;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void render(GraphicsContext gc) {
+        gc.fillRect(this.xPos,this.yPos,32,32);
+    }
+}
---
+++ src/main/java/lu/innocence/ignis/engine/EventLayer.java
@@ -0,0 +1,34 @@
+package lu.innocence.ignis.engine;
+
+import javafx.scene.canvas.GraphicsContext;
+
+/**
+ * @author Fabien Steines
+ *         mailto:fabien.steines@post.lu
+ *         Copyright by POST Technologies
+ *         <p>
+ *         <p>
+ *         Last revision - $(DATE) - Fabien Steines
+ */
+public class EventLayer {
+
+    private int height;
+    private int width;
+
+    /**
+     *
+     */
+    public EventLayer() {
+        this.width = 0;
+        this.height = 0;
+    }
+
+    /**
+     *
+     * @param gc
+     */
+    public void render(GraphicsContext gc) {
+
+    }
+
+}
